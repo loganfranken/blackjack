e39baf1e44790aa28c47720c6ac85a241dc3dd22
Logan Franken <loganfranken@gmail.com>
2017-11-18 14:53:02 -0800

    Added in prototype messaging system and card styling

--- script/main.js
+++ script/main.js
@@ -1,233 +1,108 @@
-const Deck = require('./Deck.js');
-const Hand = require('./Hand.js');
-const readline = require('readline');
-const RoundEndCondition = require('./RoundEndCondition.js');
-const RoundEndState = require('./RoundEndState.js');
+const messages = [
+  "Hiya! My name's *Chip*! Let's play some *Blackjack*!",
+  "This is the second line of dialogue!",
+  "This is the third line of dialogue!"
+];
 
-const bet = 5;
+const dealerMessage = document.getElementById('dealer-message');
 
-let playerPot = 100;
-let deck = new Deck();
-let playerHand = null;
-let dealerHand = null;
+let isTalking = false;
+let isSkipping = false;
+let currMessageIndex = 0;
 
-const displayHand = (name, hand) => {
-  let output = `${name}: `;
+const outputMessage = (message) => {
+  return new Promise((resolve, reject) => {
 
-  hand.cards.forEach((card) => {
-    output += (`[${card.isFaceUp ? card.rank : '#'}] `);
+    let currIndex = 0;
+    const length = message.length;
+
+    const outputNextCharacter = () => {
+
+        if(isSkipping)
+        {
+          dealerMessage.innerHTML = convertMessageToHtml(message);
+          isSkipping = false;
+          resolve();
+          return;
+        }
+
+        const character = message[currIndex];
+        dealerMessage.innerHTML = (convertMessageToHtml(message.slice(0, currIndex + 1)));
+        currIndex++;
+
+        let timeout = 50;
+        if(character === '.' || character === '?' || character === '!')
+        {
+          timeout = 200;
+        }
+
+        if(currIndex < length)
+        {
+          setTimeout(outputNextCharacter, timeout);
+        }
+        else
+        {
+          resolve();
+        }
+
+    };
+
+    outputNextCharacter();
+
   });
-
-  output += ` => ${hand.getPipTotal()}`;
-
-  console.log(output);
 };
 
-const displayHands = () => {
-  displayHand('Player', playerHand);
-  displayHand('Dealer', dealerHand);
-  console.log('');
-};
-
-const startRound = () => {
-
-  console.log('');
-  console.log('==== START ROUND ====')
-  console.log(`Player Pot: ${playerPot}`);
-  console.log(`Cards Left in Deck: ${deck.cards.length}`);
-  console.log('');
-
-  playerHand = new Hand();
-  dealerHand = new Hand();
-
-  // Deal face-up card to the player
-  playerHand.takeCard(deck.dealFaceUpCard());
-
-  // Deal face-up card to the dealer
-  dealerHand.takeCard(deck.dealFaceUpCard());
-
-  // Deal another face-up card to the player
-  playerHand.takeCard(deck.dealFaceUpCard());
-
-  // Finally, deal a face-down card to the dealer
-  dealerHand.takeCard(deck.dealFaceDownCard());
-
-  displayHands();
-
-  if(dealerHand.getPipTotal(true) === 21)
+document.addEventListener('keydown', (event) => {
+  if(event.keyCode === 13)
   {
-    handleRoundEnd(RoundEndState.DealerWins, RoundEndCondition.NaturalBlackjack);
-    return;
-  }
-
-  if(playerHand.getPipTotal() === 21)
-  {
-    handleRoundEnd(RoundEndState.PlayerWins, RoundEndCondition.NaturalBlackjack);
-    return;
-  }
-
-  promptPlayer();
-
-};
-
-const promptPlayer = () => {
-
-  const prompt = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  prompt.question('[H]: Hit, [S]: Stand ', (answer) => {
-
-    prompt.close();
-
-    switch(answer)
+    if(!isTalking)
     {
-      case 'H':
-        handlePlayerHit();
-        break;
-
-      case 'S':
-        handlePlayerStand();
-        break;
-
-      default:
-        console.log('Invalid input');
-        promptPlayer();
-        break;
+      outputNextMessage();
     }
+    else if(!isSkipping)
+    {
+      isSkipping = true;
+    }
+  }
+});
+
+const outputNextMessage = () => {
+
+  if(currMessageIndex >= messages.length)
+  {
+    return;
+  }
+
+  dealerMessage.innerHTML = '';
+
+  isTalking = true;
+  outputMessage(messages[currMessageIndex++]).then(() => {
+    isTalking = false;
   });
 
 };
 
-const handlePlayerHit = () => {
+convertMessageToHtml = (message) => {
 
-  // Deal face-up card to the player
-  playerHand.takeCard(deck.dealFaceUpCard());
-  displayHands();
+  let isStrong = false;
 
-  if(playerHand.getPipTotal() === 21)
+  let outputHtml = '';
+  for(let i=0; i<message.length; i++)
   {
-    handleRoundEnd(RoundEndState.PlayerWins, RoundEndCondition.Blackjack);
-    return;
-  }
+    const currLetter = message[i];
 
-  if(playerHand.getPipTotal() > 21)
-  {
-    handleRoundEnd(RoundEndState.DealerWins, RoundEndCondition.Busted);
-    return;
-  }
-
-  promptPlayer();
-
-};
-
-const handlePlayerStand = () => {
-
-  if(!dealerHand.cards[1].isFaceUp)
-  {
-    // First, reveal the dealer's hole card
-    dealerHand.cards[1].isFaceUp = true;
-  }
-  else
-  {
-    // After revealing the hole card, draw a face-up card
-    dealerHand.takeCard(deck.dealFaceUpCard());
-  }
-
-  displayHands();
-
-  if(dealerHand.getPipTotal() === 21)
-  {
-    handleRoundEnd(RoundEndState.DealerWins, RoundEndCondition.Blackjack);
-    return;
-  }
-
-  if(dealerHand.getPipTotal() > 21)
-  {
-    handleRoundEnd(RoundEndState.PlayerWins, RoundEndCondition.Busted);
-    return;
-  }
-
-  if(dealerHand.getPipTotal() > 17)
-  {
-    if(dealerHand.getPipTotal() > playerHand.getPipTotal())
+    if(currLetter === '*')
     {
-      handleRoundEnd(RoundEndState.DealerWins, RoundEndCondition.HigherScore);
-    }
-    else if(playerHand.getPipTotal() > dealerHand.getPipTotal())
-    {
-      handleRoundEnd(RoundEndState.PlayerWins, RoundEndCondition.HigherScore);
+      outputHtml += (!isStrong) ? '<strong>' : '</strong>';
+      isStrong = !isStrong;
     }
     else
     {
-      handleRoundEnd(RoundEndState.Tie);
+      outputHtml += currLetter;
     }
-
-    return;
   }
-
-  handlePlayerStand();
+  return outputHtml;
 
 };
 
-const handleRoundEnd = (roundEndState, roundEndCondition) => {
-
-  if(roundEndState == RoundEndState.DealerWins)
-  {
-    playerPot -= bet;
-
-    if(roundEndCondition === RoundEndCondition.NaturalBlackjack)
-    {
-      console.log('Dealer started with a natural blackjack. Dealer wins!');
-    }
-    else if(roundEndCondition === RoundEndCondition.Blackjack)
-    {
-      console.log('Dealer got a blackjack. Dealer wins!');
-    }
-    else if(roundEndCondition === RoundEndCondition.Busted)
-    {
-      console.log('Player busted. Dealer wins!');
-    }
-    else if(roundEndCondition === RoundEndCondition.HigherScore)
-    {
-      console.log('Dealer has higher score than player. Dealer wins!');
-    }
-  }
-  else if(roundEndState === RoundEndState.PlayerWins)
-  {
-    playerPot += (bet * 2);
-
-    if(roundEndCondition === RoundEndCondition.NaturalBlackjack)
-    {
-      console.log('Player started with a natural blackjack. Player wins!');
-    }
-    else if(roundEndCondition === RoundEndCondition.Blackjack)
-    {
-      console.log('Player got a blackjack. Player wins!');
-    }
-    else if(roundEndCondition === RoundEndCondition.Busted)
-    {
-      console.log('Dealer busted. Player wins!');
-    }
-    else if(roundEndCondition === RoundEndCondition.HigherScore)
-    {
-      console.log('Player has higher score than dealer. Player wins!');
-    }
-  }
-  else if(roundEndState === RoundEndState.Tie)
-  {
-    console.log('Tie!');
-  }
-
-  if(deck.cards.length < 15)
-  {
-    // If the deck has less than 15 cards, reset the deck
-    deck = new Deck();
-  }
-
-  startRound();
-
-};
-
-startRound();
+//outputNextMessage();
