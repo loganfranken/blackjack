9cc999bccc7807c563fbe1e470e5be466e3ee4f0
Logan Franken <loganfranken@gmail.com>
2019-01-13 21:39:00 -0800

    Fixed typo in initial stand explanation

--- script/actions/chipPlayerChoice.js
+++ script/actions/chipPlayerChoice.js
@@ -101,7 +101,7 @@ const dialogChoices = [
     action: async () => {
       await chip("Now you get to make a *choice*...");
       await chip("You can *hit* and take another card.");
-      await chip("Or you can *stand* and I'll start dealing myself cards until I reach *17*.");
+      await chip("Or you can *stand* and I'll start dealing myself cards until I go past *17*.");
       await chip("Sooo, do you want to hit or stand? (*Choose below*)", true);
       return getStandardPlayerChoice();
     }
